1fc3b382e8c6534e71725dd66cff783513069a9d
joaobatistafrontend <jbbuno007@gmail.com>
2022-10-22 12:40:27 -0300

    atualizado, fazer o pusher e mellhorar o teomplate

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,17 @@ var sec = 60
 var interval
 
 function towDigits(digit){
+     if (min < 3) {
+          document.getElementById('stopwatch').style.color='green'
+     }
+     if (min < 1){
+          document.getElementById('stopwatch').style.color='red'
+     }
      if(digit<10){
           return('0'+digit)
-     }else{
+          
+     }
+     else{
           return(digit)
      }
 }
@@ -48,4 +56,23 @@ function watch(){
      console.log(min)
      console.log(sec)
      document.getElementById('stopwatch').innerHTML =towDigits(min) + ':' + towDigits(sec)
+}
+
+function one(){
+     min ++
+     
+     // document.getElementById('stopwatch').innerHTML =towDigits(min) + ':00' + towDigits(sec)
+
+}
+function two(){
+     min += 2
+}
+function three(){
+     min += 3
+}
+function four(){
+     min += 4
+}
+function five(){
+     min += 5
 }
